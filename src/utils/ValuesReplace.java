7d871010ed695ee63a12a144a342dbb169b502ce
grata <grata.salve@gmail.com>
2022-09-13 17:04:06 +0300

    Initial commit

--- src/utils/ValuesReplace.java
+++ src/utils/ValuesReplace.java
@@ -1,46 +1,93 @@
 package utils;
 
-import java.util.Arrays;
 import java.util.Stack;
 
 public class ValuesReplace {
-    private static Object[] numbers;
+    private static Integer[] numbers;
     private static Stack<Object> stack = new Stack<>();
     private static int iterator = 0;
 
     public static void printNumbersWithReplace(int n) {
-        numbers = new Object[n + 1];
-        for (int i = 0; i <= n; i++) {
-            numbers[i] = i;
+        numbers = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = i + 1;
         }
 
-        //Arrays.stream(numbers).forEach(s -> System.out.print(s + " "));
-
         Thread A = new Thread(() -> {
-            if ((int) numbers[iterator] % 3 == 0)
-                stack.push("fizz");
-        });
-
-        Thread B = new Thread(() -> {
-            if ((int) numbers[iterator] % 5 == 0)
-                stack.push("buzz");
-        });
-
-        Thread D = new Thread(() -> {
-            while (iterator <= n) {
-                A.start();
-                B.start();
-                if (!stack.isEmpty()) {
-                    System.out.print(stack.pop() + " ");
-                } else System.out.print(numbers[iterator]);
-                A.stop();
-                B.stop();
-                iterator++;
+            synchronized (numbers) {
+                for (Integer num : numbers) {
+                    if (num != 0 && num % 3 == 0 && num % 5 != 0)
+                        stack.push("fizz");
+                    try {
+                        while (iterator != num)
+                            numbers.wait(0,100);
+                        numbers.notify();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         });
 
+        Thread B = new Thread(() -> {
+            synchronized (numbers) {
+                for (Integer num : numbers) {
+                    if (num != 0 && num % 5 == 0 && num % 3 != 0)
+                        stack.push("buzz");
+                    try {
+                        while (iterator != num)
+                            numbers.wait(0,100);
+                        numbers.notify();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        Thread C = new Thread(() -> {
+            synchronized (numbers) {
+                for (Integer num : numbers) {
+                    if (num != 0 && num % 5 == 0 && num % 3 == 0)
+                        stack.push("fizzbuzz");
+                    try {
+                        while (iterator != num)
+                            numbers.wait(0,100);
+                        numbers.notify();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        Thread D = new Thread(() -> {
+            A.start();
+            B.start();
+            C.start();
+            while (iterator < n) {
+                synchronized (stack) {
+                    try {
+                        stack.wait(30);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    number();
+                    iterator++;
+                }
+            }
+        });
 
         D.start();
+    }
 
+    private static void number() {
+        if (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        } else {
+            System.out.print(numbers[iterator] + " ");
+        }
     }
 }
+
+
